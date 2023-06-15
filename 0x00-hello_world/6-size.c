1f6500ab85e2b23d76a1e345e99083fe821bb515
TatendaTorerwa <ttorerwa01@gmail.com>
2023-06-15 10:20:47 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,9 @@ int main(void)
 {
 printf("size of a char:%lu bytes(s)\n", sizeof(char));
 printf("size of an int:%lu bytes(s)\n", sizeof(int));
-printf("size of a long int:%lu bytes\n", sizeof(long int));
-printf("size of a long long int:%lu bytes\n", sizeof(long long int));
-printf("size of a float:%lu bytes\n", sizeof(float));
+printf("size of a long int:%lu bytes(s)\n", sizeof(long int));
+printf("size of a long long int:%lu bytes(s)\n", sizeof(long long int));
+printf("size of a float:%lu bytes(s)\n", sizeof(float));
 
 return (0);
 
