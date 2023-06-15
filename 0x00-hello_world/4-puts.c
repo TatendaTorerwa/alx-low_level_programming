e43cfce510a6830d89a3b798e260c7ac81a347de
TatendaTorerwa <ttorerwa01@gmail.com>
2023-06-15 08:43:22 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
 *main - main entry point
-*return value always 0 (success)
+*Return:  value 0 is considered as successful execution
 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
+
 return (0);
 
 }
