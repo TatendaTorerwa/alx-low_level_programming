35b9e3f6eac7d13e1ac02b5c795007bd70b51e79
TatendaTorerwa <ttorerwa01@gmail.com>
2023-06-15 07:16:30 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,9 @@
 * main - main entry point
 *return : always 0 (success)
 */
-
 int main(void)
 {
- puts ("\"Programming is like building a multilingual puzzle,");
+ puts ("\"Programming is like building a multilingual puzzle");
  return (0);
 
 }
