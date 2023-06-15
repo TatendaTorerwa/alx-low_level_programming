ba8571451c13d1e776fd79505f5aa7ceea88abdf
TatendaTorerwa <ttorerwa01@gmail.com>
2023-06-15 10:04:42 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
 *main - Entry point
-*return: Value 0 is considered as successful execution.
+*return:Value 0 is considered as successful execution.
 */
 int main(void)
 {
-printf("size of char:%lu bytes(s)\n",sizeof(char));
-printf("size of int:%lu bytes(s)\n",sizeof(int));
-printf("size of long int:%lu bytes\n",sizeof(long int));
-printf("size of long long int:%lu bytes\n",sizeof(long long int));
-printf("size of float:%lu bytes\n",sizeof(float));
+printf("size of char:%lu bytes(s)\n", sizeof(char));
+printf("size of int:%lu bytes(s)\n", sizeof(int));
+printf("size of long int:%lu bytes\n", sizeof(long int));
+printf("size of long long int:%lu bytes\n", sizeof(long long int));
+printf("size of float:%lu bytes\n", sizeof(float));
 
 return (0);
 
