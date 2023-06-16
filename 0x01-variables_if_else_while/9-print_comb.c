4860cb7a9ccca8a2dfea7a916707611b4051ea4d
TatendaTorerwa <ttorerwa01@gmail.com>
2023-06-16 08:59:15 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*Description: " prints all possible combinations of single-digit numbers"
+*
+*Return: always 0 (success)
+*/
+
+int main(void)
+
+{
+
+int z;
+
+for (z = 48; z <= 57; z++)
+
+{
+putchar(z);
+putchar(',');
+putchar(' ');
+
+}
+putchar('\n');
+
+return (0);
+
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -7,3 +7,4 @@ Numbers.
 Numberz.
 Smile in the mirror.
 Hexadecimal.
+Patience, persistence and perspiration make an unbeatable combination for success.
