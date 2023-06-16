f19f413e4fb2bcb02702b230b227df7d7d3b18d6
TatendaTorerwa <ttorerwa01@gmail.com>
2023-06-16 08:06:25 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*
+*Description: " prints all single digit numbers of base 10"
+*
+*Return: always 0 (success)
+*/
+
+int main(void)
+
+{
+
+for (n = 0; n <= 9; n++)
+{
+printf("%d\n", n);
+}
+
+return (0);
+
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -3,3 +3,4 @@ The last digit.
 I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game.
 alphABET.
 When I was having that alphabet soup, I never thought that it would pay off.
+Numbers.
