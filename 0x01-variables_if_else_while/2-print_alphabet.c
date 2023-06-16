28f1a692a33ba101cc6b06c00b7e4dd9ed5cfc6f
TatendaTorerwa <ttorerwa01@gmail.com>
2023-06-16 07:25:09 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*
+*Description: "prints the alphabet in lowercase"
+*
+*Return: always 0 (success)
+*/
+
+int main(void)
+
+{
+int x = 97;
+
+while (x < 122)
+{
+putchar(x);
+x++;
+}
+
+putchar('\n');
+
+return (0);
+
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,3 @@
 Positive anything is better than negative nothing.
-The last digi.
+The last digit.
+I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game.
