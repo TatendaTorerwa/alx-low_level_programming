bfed5385bcb2a13f9e725ec2a5c5a75ea7f49c8a
TatendaTorerwa <ttorerwa01@gmail.com>
2023-06-20 01:06:03 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*print_alphabet - prints alphabet in lowercase
+*
+*Description: "prints the alphabet, in lowercase"
+*
+*Return: (void)
+*/
+
+void print_alphabet(void)
+
+{
+int a;
+
+for (a = 97; a <= 122; a++)
+
+{
+_putchar(a);
+
+}
+
+_putchar('\n');
+
+}
+
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,3 @@
-0x02-functions_nested_loops
+functions,nested loops.
+0. _putchar.
+1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game.
