e280893b5fa6658f5c9fedac229c0f126554e042
TatendaTorerwa <ttorerwa01@gmail.com>
2023-06-20 01:23:12 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 -  prints 10 times the alphabet, in lowercase
+*
+*Description: " prints 10 times the alphabet, in lowercase"
+*
+*Return: (void)
+*/
+
+void print_alphabet_x10(void)
+
+{
+int a = 0;
+int b = 97;
+
+for (a = 0; a <= 10; a++)
+
+{
+for (b = 97; b <= 122; b++)
+
+{
+_putchar(b);
+
+}
+
+_putchar('\n');
+
+}
+
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 functions,nested loops.
 0. _putchar.
 1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game.
+2. 10 x alphabet.
