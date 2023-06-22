8186cfb39b76a18bf45f1828a758885615a5cc17
TatendaTorerwa <ttorerwa01@gmail.com>
2023-06-22 02:14:24 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*
+*Description: "prints the numbers from 1 to 100"
+*
+*Return: always 0 (success)
+*/
+
+int main(void)
+
+{
+
+	int m;
+
+	for (m = 1; m <= 100; m++)
+	{
+		if ((m % 3 == 0) || (m % 5 == 0))
+
+		{
+			printf("FizzBuzz");
+		}
+
+		else if (m % 3 == 0)
+		{
+			printf("Fizz");
+		}
+
+		else if (m % 5 == 0)
+		{
+
+			printf("Buzz");
+		}
+
+		else
+
+		{
+
+			printf("%d", m);
+
+		}
+
+			if (m != 100)
+
+			{
+				printf(" ");
+			}
+
+	}
+		printf("\n");
+		return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,3 +6,4 @@ void print_numbers(void);
 void print_most_numbers(void);
 void print_line(int n);
 void print_square(int size);
+
