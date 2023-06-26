00c75a30b735ee3565191778b0626277cadc3096
TatendaTorerwa <ttorerwa01@gmail.com>
2023-06-26 06:30:04 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -19,7 +19,16 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d, ", a[i]);
+		printf("%d", a[i]);
+
+		if (i == n - 1)
+		{
+			break;
+		}
+		else
+		{
+			printf(", ");
+		}
 	}
 	printf("\n");
 }
