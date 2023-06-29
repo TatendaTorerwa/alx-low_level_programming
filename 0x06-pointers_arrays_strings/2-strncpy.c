d68780a8c9239da954a1aefebcd877fc66e075bf
TatendaTorerwa <ttorerwa01@gmail.com>
2023-06-29 02:17:54 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -17,4 +17,15 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 
-	
+	int r;
+	for (r = 0; r < n && src[r] != '\0'; r++)
+	{
+		dest[r] = src[r];
+	}
+	for (; r < n; r++)
+	{
+		dest[r] = '\0';
+	}
+
+	return (dest);
+}
