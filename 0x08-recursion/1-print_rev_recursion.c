78eb2fab03f13a16db52ea10da4f8b9ba56db7e3
TatendaTorerwa <ttorerwa01@gmail.com>
2023-07-05 01:22:57 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*_print_rev_recursion -  prints a string in reverse
+*
+*@s: pointer
+*
+*Description: " prints a string in reverse"
+*
+*Return: (void)
+*/
+
+void _print_rev_recursion(char *s)
+
+{
+
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		s++;
+		_print_rev_recursion(s);
+		s--;
+		_putchar(*s);
+	}
+}
