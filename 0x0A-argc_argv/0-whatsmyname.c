7f1558f6e55c54c25595899ce17a3e705bd8870e
TatendaTorerwa <ttorerwa01@gmail.com>
2023-07-07 00:13:34 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+*main - prints its name, followed by a new line
+*
+*@argc: argument count
+*
+*@argv: argument vector
+*
+*Description: "prints its name, followed by a new line"
+*
+*Return: always 0 (success)
+*/
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
