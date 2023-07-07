1583a73c403bbb49d2dab7afe03285a3bf17ddad
TatendaTorerwa <ttorerwa01@gmail.com>
2023-07-07 03:45:29 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 	{
 		num = atoi(argv[i]);
 
-		if (num <= 0 && argv[1][0] != '0')
+		if (num <= 0 && argv[1][0] != '\0')
 		{
 			printf("Error\n");
 			return (1);
