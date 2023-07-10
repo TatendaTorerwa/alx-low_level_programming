90cb9920d1d9472fbef2ad93a58c6254090878d2
TatendaTorerwa <ttorerwa01@gmail.com>
2023-07-10 07:11:13 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,7 +8,7 @@
 *
 *@s1: pointer string
 *
-*s2: pointer string
+*@s2: pointer string
 *
 *Description: "concatenates two strings"
 *
@@ -17,45 +17,27 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int m = 0, n = 0;
-	int i, j;
+	int m = 0, n = 0, i, j;
 	char *ptr;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 	while (s1[m] != '\0')
-	{
 		m++;
-	}
 	while (s2[n] != '\0')
-	{
 		n++;
-	}
 
 	ptr = malloc(sizeof(char) * (m + n + 1));
 
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
 	for (i = 0; i < m; i++)
-	{
 		ptr[i] = s1[i];
-	}
 
 	for (j = 0; j < n; j++)
-	{
 		ptr[i + j] = s2[j];
-	}
-
 	ptr[i + j] = '\0';
-
-
 	return (ptr);
 }
