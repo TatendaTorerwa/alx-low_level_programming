327cdb4397362c8db80f300e7faf2d66d4195766
TatendaTorerwa <ttorerwa01@gmail.com>
2023-07-10 04:04:39 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,1 +1,46 @@
-#include <stdio
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+*_strdup -  duplicate to new memory space location
+*
+*@str: char pointer
+*
+*Description: "duplicate to new memory space location"
+*
+*Return: pointer
+*/
+
+char *_strdup(char *str)
+{
+	int m = 0, n;
+	char *ptr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[m] != '\0')
+	{
+		m++;
+	}
+
+	ptr = malloc(sizeof(char) * (m + 1));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (n = 0; n < m; n++)
+	{
+		ptr[n] = str[n];
+	}
+
+	return (ptr);
+
+	free(ptr);
+}
