047f62ab1503d84623cfee991b9533c3c6ec06f9
TatendaTorerwa <ttorerwa01@gmail.com>
2023-07-17 04:42:38 -0700

    dog.h

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+*_strlen - returns the length of a string
+*
+*@str: string to calculate
+*
+*Return: the length of the string
+*/
+
+
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -29,6 +29,6 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
+int _strlen(char *str);
 
 #endif
