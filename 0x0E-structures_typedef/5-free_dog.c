367e1d1668563e961f2ed8fff8973b75af057da6
TatendaTorerwa <ttorerwa01@gmail.com>
2023-07-17 05:15:25 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+*free_dog - frees dogs
+*
+*@d: struct dog to free
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
