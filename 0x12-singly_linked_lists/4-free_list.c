1055c0eed7575ef4674ce00ba70fb7565474e2eb
TatendaTorerwa <ttorerwa01@gmail.com>
2023-07-27 06:11:01 -0700

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,11 +1,10 @@
 #include <stdlib.h>
-#include <string.h>
 #include "lists.h"
 
 /**
 *free_list - frees a linked list
 *
-*list_t list to be freed
+*@head: list_t list to be freed
 */
 
 void free_list(list_t *head)
