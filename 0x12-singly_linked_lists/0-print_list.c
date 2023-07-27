8a21b88d57523f0614a13268e83e452d2c60e155
TatendaTorerwa <ttorerwa01@gmail.com>
2023-07-27 07:23:07 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,7 +11,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
+	size_t node = 0;
 
 	while (h)
 	{
@@ -20,8 +20,8 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		s++;
+		node++;
 	}
 
-	return (s);
+	return (node);
 }
