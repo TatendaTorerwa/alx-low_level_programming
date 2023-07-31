aaf5c3ec73d1336eabc9e86e5363d12d44e7dd0f
TatendaTorerwa <ttorerwa01@gmail.com>
2023-07-31 09:19:47 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+*find_listint_loop - finds the loop in a linked list.
+*
+*@head: linked list to search for
+*
+*Return: address of the node where the loop starts, or NULL
+*/
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint *p, *q;
+
+	p = head;
+	q = head;
+
+	while (p && q && q->next)
+	{
+		p = p->next;
+		q = q->next->next;
+		if (p == q)
+		{
+			return (p);
+		}
+	}
+	return (NULL);
+}
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,9 +12,9 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp, *currentnode;
 
-	temp = currentnode = *head;
+	currentnode = *head;
 
-	if (*head == NULL)
+	if (head == NULL || *head == NULL)
 	{
 		return;
 	}
@@ -28,8 +28,6 @@ void free_listint2(listint_t **head)
 			free(temp);
 		}
 
-		currentnode = NULL;
-		temp = NULL;
 		*head = NULL;
 	}
 }
