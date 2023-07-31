ba2c0ffac549c33707cfaffb4162260afb845e72
TatendaTorerwa <ttorerwa01@gmail.com>
2023-07-31 09:37:27 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,13 +13,12 @@ void free_listint2(listint_t **head)
 	listint_t *temp;
 
 	if (head == NULL)
-	{
 		return;
-	}
+
 	while (*head)
 	{
 		temp = (*head)->next;
-		free(head);
+		free(*head);
 		*head = temp;
 	}
 
