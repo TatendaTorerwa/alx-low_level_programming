fecc921240ace6a505d60cb48a313d0d444020a9
TatendaTorerwa <ttorerwa01@gmail.com>
2023-10-12 06:40:25 -0700

    Requirements.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,11 +17,18 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp;
 	dlistint_t *newnode;
 	unsigned int position = 0;
+	unsigned int length = 0;
 
 	newnode = malloc(sizeof(dlistint_t));
 	if (newnode == NULL)
 	{
 		return (NULL);
+	}
+
+	if (*h == NULL)
+	{
+		h = &newnode;
+		return (*h);
 	}
 
 	newnode->n = n;
@@ -33,6 +40,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		while (temp->prev != NULL)
 		{
 			temp = temp->prev;
+			length++;
+		}
+		
+		if (idx > length - 1)
+		{
+			return (NULL);
 		}
 
 		while (position < idx - 1)
