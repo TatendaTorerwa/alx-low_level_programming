d8c2890bef0814527783c9b3622dda05df9008d6
TatendaTorerwa <ttorerwa01@gmail.com>
2023-11-03 02:03:07 -0700

    0-hash_table_create.c .

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,31 +11,18 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
-	unsigned long int p;
-
-	if (size == 0)
-	{
-		return (NULL);
-	}
+	unsigned long int i;
 
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
-	{
 		return (NULL);
-	}
 
 	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
-
 	if (ht->array == NULL)
-	{
 		return (NULL);
-	}
-
-	for (p = 0; p < size; p++)
-	{
-		ht->array[p] = NULL;
-	}
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
 
 	return (ht);
 }
