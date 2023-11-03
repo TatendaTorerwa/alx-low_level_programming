ee8ad81477fa66d7cc54c417133ec994b02fe432
TatendaTorerwa <ttorerwa01@gmail.com>
2023-11-03 01:27:22 -0700

    0. >>> ht = {}.

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table.
+ *
+ * @size: The size of the array.
+ *
+ * Return: A pointer to the newly created hash table.
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int p;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+	{
+		return (NULL);
+	}
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+
+	for (p = 0; p < size; p++)
+	{
+		ht->array[p] = NULL;
+	}
+
+	return (ht);
+}
