cf7337c4dcd825c9b7afcedc4542469ed81fa5f7
TatendaTorerwa <ttorerwa01@gmail.com>
2024-02-05 13:09:09 -0800

    0. Linear search.

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * linear_search - Searches for a value in an array using the Linear search algorithm.
+ * linear_search - Searches for a value using the Linear search algorithm.
  *
  * @array:  Pointer to the first element of the array to search.
  *
